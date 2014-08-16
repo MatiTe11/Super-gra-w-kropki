a4362f7a4d4d59580f4e92bbafd85070cc4067bc
MatiTe11 <m.tegowski@wp.pl>
2014-08-17 00:25:34 +0200

    nowa warsja

--- Super gra/Super gra/Game1.cs
+++ Super gra/Super gra/Game1.cs
@@ -14,6 +14,7 @@ namespace Super_gra
     /// <summary>
     /// This is the main type for your game
     /// </summary>
+    /// dhsgggggsjgfjyfgs
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
