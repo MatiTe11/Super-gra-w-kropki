c933ea0bcffb0a56438f25014e837c6216b15700
feniks611 <buhaha.ja@gmail.com>
2014-08-17 13:11:23 +0200

    znowu update koloru planszy

--- Super gra/Super gra/Game1.cs
+++ Super gra/Super gra/Game1.cs
@@ -84,7 +84,7 @@ namespace Super_gra
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Grey);
+            GraphicsDevice.Clear(Color.Gray);
 
             // TODO: Add your drawing code here
 
