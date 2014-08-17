91a5f9eecd3dbe303f7e028b15a7de995f490e46
MatiTe11 <m.tegowski@wp.pl>
2014-08-17 11:44:43 +0200

    czarny

--- Super gra/Super gra/Game1.cs
+++ Super gra/Super gra/Game1.cs
@@ -84,7 +84,7 @@ namespace Super_gra
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
