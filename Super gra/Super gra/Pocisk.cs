4578f29414446ef53276450d081514fd862f63cb
feniks611 <buhaha.ja@gmail.com>
2014-08-17 14:14:35 +0200

    zmiany w pocisku

--- Super gra/Super gra/Pocisk.cs
+++ Super gra/Super gra/Pocisk.cs
@@ -12,17 +12,17 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Super_gra
 {
-    class Pocisk
+   public class Pocisk
     {
 
         const int zasieg = 500;
 
         public bool Visible = false;
 
-        Vector2 pozycja_startowa;
-        Vector2 predkosc;
-        Vector2 kierunek;
-        Texture2D pocisk;
+       public Vector2 pozycja;
+       public Vector2 predkosc;
+       public Vector2 kierunek;
+       public Texture2D pocisk;
 
         public void LoadContent(ContentManager Content)
         {
@@ -30,5 +30,12 @@ namespace Super_gra
         }
 
         ///koment
+        ///
+
+        public void Draw(SpriteBatch Gracz)
+        {
+            Gracz.Draw(texture,pozycja,null,)
+        }
+
     }
 }
